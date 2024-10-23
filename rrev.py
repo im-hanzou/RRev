801b214b5f42474988a10811ff73d85310be17cf
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2024-10-23 16:57:32 +0700

    update rrev.py
    using a new method to speed up the process

--- rrev.py
+++ rrev.py
@@ -1,7 +1,8 @@
 import os
 import re
-import threading
-import requests
+import pycurl
+import io
+from multiprocessing import Pool, Manager
 from fake_useragent import UserAgent
 from colorama import Fore, Style, init
 
@@ -11,11 +12,11 @@ def print_banner():
     banner = f"""
 {Fore.CYAN}
       _____  _____            
-     |  __ \|  __ \           
+     |  __ \\|  __ \\           
      | |__) | |__) |_____   __
-     |  _  /|  _  // _ \ \ / /
-     | | \ \| | \ \  __/\ V / 
-     |_|  \_\_|  \_\___| \_/  
+     |  _  /|  _  // _ \\ \\ / /
+     | | \\ \\| | \\ \\  __/\\ V / 
+     |_|  \\_\\_|  \\_\\___| \\_/  
                           
         {Fore.RED}RapidDNS ReverseIP
         Github: IM-Hanzou
@@ -26,27 +27,42 @@ def print_banner():
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def process_reversing(ips):
+def fetch_content(url, header):
+    buffer = io.BytesIO()
+    curl = pycurl.Curl()
+    curl.setopt(curl.URL, url)
+    curl.setopt(curl.HTTPHEADER, header)
+    curl.setopt(curl.WRITEDATA, buffer)
     try:
-        for ip in ips:
-            ip = ip.strip()
-            if ip.startswith(('http://', 'https://')):
-                ip = re.sub(r'^https?://', '', ip)
-            response = session.get(f'https://rapiddns.io/s/{ip}?full=1#result', headers=header, verify=True).content.decode('utf-8')
-            results = re.findall(r'</th>\n<td>(.*?)</td>', response)
-            num_results = len(results)
-            status = 'Failed' if num_results == 0 else num_results
-            color = Fore.GREEN if num_results > 0 else Fore.RED
-            print(f'{color}[#] Reversed {Fore.MAGENTA}{ip}{Style.RESET_ALL} {color}=>{Style.RESET_ALL} {Fore.YELLOW}{status}{Style.RESET_ALL} {color}domains [#]{Style.RESET_ALL}')
-            
-            for result in results:
-                result = result.strip()
-                if result.startswith('www.'):
-                    result = result[4:]
-                if result not in domain_list:
-                    domain_list.append(result)
-                    with open('results.txt', 'a+') as file:
-                        file.write(result + '\n')
+        curl.perform()
+        curl.close()
+        return buffer.getvalue().decode('utf-8')
+    except pycurl.error as e:
+        curl.close()
+        return str(e)
+
+def process_reversing(ip, domain_list, header):
+    try:
+        ip = ip.strip()
+        if ip.startswith(('http://', 'https://')):
+            ip = re.sub(r'^https?://', '', ip)
+        
+        url = f'https://rapiddns.io/s/{ip}?full=1#result'
+        response = fetch_content(url, header)
+        results = re.findall(r'</th>\n<td>(.*?)</td>', response)
+        num_results = len(results)
+        status = 'Failed' if num_results == 0 else num_results
+        color = Fore.GREEN if num_results > 0 else Fore.RED
+        print(f'{color}[#] Reversed {Fore.MAGENTA}{ip}{Style.RESET_ALL} {color}=>{Style.RESET_ALL} {Fore.YELLOW}{status}{Style.RESET_ALL} {color}domains [#]{Style.RESET_ALL}')
+        
+        for result in results:
+            result = result.strip()
+            if result.startswith('www.'):
+                result = result[4:]
+            if result not in domain_list:
+                domain_list.append(result)
+                with open('results.txt', 'a+') as file:
+                    file.write(result + '\n')
     except Exception as e:
         print(f'{Fore.RED}Error: {e}{Style.RESET_ALL}')
 
@@ -58,24 +74,17 @@ def start_reverse_lookup():
         with open(ip_list_file, 'r') as file:
             lines = file.read().splitlines()
         
-        thread_size = 50
-        coks = [lines[i:i + thread_size] for i in range(0, len(lines), thread_size)]
-        threads = []
-        for cok in coks:
-            thread = threading.Thread(target=process_reversing, args=(cok,))
-            threads.append(thread)
-            thread.start()
+        thread_size = 20
+        header = [f'User-Agent: {UserAgent().random}']
         
-        for thread in threads:
-            thread.join()
-        
+        with Manager() as manager:
+            domain_list = manager.list()
+            with Pool(thread_size) as pool:
+                pool.starmap(process_reversing, [(ip, domain_list, header) for ip in lines])
+
         print(f"{Fore.LIGHTYELLOW_EX}\nReverse IP Lookup completed.{Style.RESET_ALL}\n{Fore.YELLOW}Results saved to {Fore.LIGHTCYAN_EX}results.txt{Style.RESET_ALL}")
     except Exception as e:
         print(f'{Fore.RED}Error: {e}{Style.RESET_ALL}')
 
 if __name__ == '__main__':
-    session = requests.Session()
-    user_agent = UserAgent()
-    header = {'User-Agent': user_agent.random}
-    domain_list = []
     start_reverse_lookup()
